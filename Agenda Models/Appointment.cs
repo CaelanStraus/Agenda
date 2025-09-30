1fdcc70dc615efaede2a3f44ebb28b8687fbf997
CaelanStraus <caelan.straus@student.ehb.be>
2025-09-30 12:07:51 +0200

    Add Agenda Models project and Appointment entity
    Introduced a new project, Agenda Models, to encapsulate the
    Appointment entity. Updated AgendaDBContext to include a
    DbSet<Appointment> for managing appointments. Configured
    Agenda Models to target .NET 9.0 with nullable reference
    types enabled. Added validation and metadata annotations
    to the Appointment class. Updated solution and project
    files to integrate the new project and establish references.

---
+++ Agenda Models/Appointment.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agenda_Models
+{
+    public class Appointment
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [Display(Name = "Vanaf")]
+        [DataType(DataType.DateTime)]
+        [Range(typeof(DateTime), "2025-01-01", "9999-12-31")]
+        public DateTime From { get; set; } = DateTime.Now + new TimeSpan(1, 0, 0, 0);
+        [Required]
+        [Display(Name = "Tot")]
+        [DataType(DataType.DateTime)]
+        public DateTime To { get; set; } = DateTime.Now + new TimeSpan(1, 0, 0, 0);
+        [Required]
+        [Display(Name = "Titel")]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        [Display(Name = "Beschrijving")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        [Display(Name = "Hele dag")]
+        public bool IsAllDay { get; set; } = false;
+        [Required]
+        [Display(Name = "Aangemaakt op")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [Required]
+        [Display(Name = "Verwijderd")]
+        public DateTime DeletedAt { get; set; } = DateTime.MaxValue;
+    }
+}
--- Agenda_Console/AgendaDBContext.cs
+++ Agenda_Console/AgendaDBContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Agenda_Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Agenda_Console
 {
     internal class AgendaDBContext : DbContext
     {
+        public DbSet<Appointment> Appointments { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=localhost;Database=AgendaDb;User Id=sa;Password=Your_password123;MultipleActiveResultSets=true");
